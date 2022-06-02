48e286495adbc2859b130f97d9a2b1b243aa3840
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-06-02 23:44:01 +0400

    Creating and placing new nodes

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,12 @@
-const link = document.querySelector('a');
-link.textContent = 'Mozilla Developer network';
-link.href = 'https://developer.mozilla.org';
+const link = document.querySelector("a");
+link.textContent = "Mozilla Developer network";
+link.href = "https://developer.mozilla.org";
+const sect = document.querySelector("section");
+const para = document.createElement("p");
+para.textContent = "We hope you enjoyed the ride.";
+sect.appendChild(para);
+const text = document.createTextNode(
+  "— the premier source for web development knowledge."
+);
+const linkPara = document.querySelector("p");
+linkPara.appendChild(text);
