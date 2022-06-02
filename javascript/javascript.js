36473163ab1238280e76adee52faba642a36b7ae
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-06-03 00:04:20 +0400

    added style to html

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -7,6 +7,7 @@ const sect = document.querySelector("section");
 const para = document.createElement("p");
 para.textContent = "We hope you enjoyed the ride.";
 sect.appendChild(para);
+
 const text = document.createTextNode(
   "— the premier source for web development knowledge."
 );
