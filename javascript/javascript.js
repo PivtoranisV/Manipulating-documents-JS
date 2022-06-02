6df8d99fe5ec77f077ca76cc1a92c5f73b922d56
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-06-02 23:55:13 +0400

    Moving and removing elemnts

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,8 @@
 const link = document.querySelector("a");
 link.textContent = "Mozilla Developer network";
 link.href = "https://developer.mozilla.org";
+
+//Creating and placing new nodes
 const sect = document.querySelector("section");
 const para = document.createElement("p");
 para.textContent = "We hope you enjoyed the ride.";
@@ -10,3 +12,7 @@ const text = document.createTextNode(
 );
 const linkPara = document.querySelector("p");
 linkPara.appendChild(text);
+
+//Moving and removing elements
+sect.appendChild(linkPara);
+linkPara.remove();
